f02a864b3dd158d55284475c6fb652fbbcc43bb7
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-01-27 17:45:53 +0900

    막힌 문, 잠긴 문

---
+++ Take Care Cat/Assets/Scripts/Object/BlindDoor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlindDoor : Object
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        name = "막힌 문";
+        explanation = "무엇을 막으려고 한 걸까?";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
