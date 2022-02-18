f6e27aa6edffad68fc070f21e2d6eb84d00867c7
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-02-18 10:05:27 +0900

    CameraController

---
+++ Take Care Cat/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Camera catCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowCatCamera()
+    {
+        mainCamera.enabled = false;
+        catCamera.enabled = true;
+    }
+}
