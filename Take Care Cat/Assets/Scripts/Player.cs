a6bbc7ba02dc6862a5ac08839558cd6da041def2
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-05-09 14:03:24 +0900

    player move + anim
    애니메이션 블랜드 사용

--- Take Care Cat/Assets/Scripts/Player.cs
+++ Take Care Cat/Assets/Scripts/Player.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] float speed;
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,17 +22,38 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    private void FixedUpdate()
+
+    void FixedUpdate()
     {
         Move();
     }
 
     private void Move()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float moveSpeed = 0;
+
+        Vector3 moveVec = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
+
+        if (moveVec.magnitude != 0)
+        {
+            float run = 1;  //달릴때의 속도 증가
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                run = 1.5f;
+                moveSpeed = 1;
+            }
+            else
+                moveSpeed = 0.5f;
+
+            transform.Translate(moveVec * speed * run * Time.fixedDeltaTime);
+        }
+        else
+            moveSpeed = 0;
+
+
+        anim.SetFloat("moveSpeed", moveSpeed);
+
     }
 }
